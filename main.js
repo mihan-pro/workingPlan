4e68f29c7508dc63d8add40af976106cea698b12
Mikhail <45981435+mihan-pro@users.noreply.github.com>
2020-03-21 20:03:05 +0300

    Revert "Add current date functions"

--- main.js
+++ main.js
@@ -5,12 +5,7 @@ function createWindow()
 {
     const win = new BrowserWindow({
         width:800,
-        height: 600,
-        minWidth: 760 ,
-        minHeight: 460,
-        title:"Учёт рабочего врмени",
-        frame:true,
-        autoHideMenuBar:true,
+        height: 800,
         webReferences:
         {
             nodeIntegration:true,
