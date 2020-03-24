76bbe56b5d030192c946df724994f730f5b77651
Mikhail <45981435+mihan-pro@users.noreply.github.com>
2020-03-24 10:08:00 +0300

    add input block

---
+++ src/js/inputblock.js
@@ -0,0 +1,11 @@
+const inputBlock = function(){
+    let startInputHTMLEl = $("#startDayInput");
+    let finsihInputHTMLEl = $("#finishDayInput");
+    let restOfTimeValueHTMLEl = $("restTime");
+    let saveDayTimeBtn =$("#saveDayTimeBtn");
+
+
+    return {
+
+    }
+}();
